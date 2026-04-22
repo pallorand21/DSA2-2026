2249ac18a98f88b8508e54e2fb1f1e34c6204d1f
Pál Loránd <pallorand21@gmail.com>
2026-04-22 10:28:43 +0300

    Parciális 1. feladata
    80% a megoldás

---
+++ Het 9/Parcialis/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int binarysearch(int arr[], int left, int right, int target);
+
+
+
+
+
+
+int rendez( const void *a, const void *b) {
+    return ( *(int*)a - *(int*)b);
+}
+
+int main(void) {
+    //I. FELADAT
+    FILE *f = fopen("1dbemenet.txt","r");
+    if (!f) {
+        printf("sikertelen 1dbemenet megnyitas");
+        exit(-2);
+    }
+    int n;
+    fscanf(f,"%d",&n);
+    int tomb[n];
+    for ( int i = 0; i < n ; ++i) {
+        fscanf(f,"%d",&tomb[i]);
+        //printf("%d\n",tomb[i]);
+    }
+    qsort(tomb,n,sizeof(int),rendez);
+    
+
+
+
+    return 0;
+}
+int binarysearch(int arr[], int left, int right, int target) {
+    int mid = (left + right) / 2;
+    if (target == arr[mid]) {
+        return mid;
+    }
+    else {
+        return binarysearch(arr, left, mid, target);
+    }
+}
